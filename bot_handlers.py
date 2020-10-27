31ea7e21b53e5f79968da8f21c204cf7ba3cc08f
Leandro Furlam Turi <leandrofturi@gmail.com>
2020-10-27 19:14:20 -0300

    comandos desconhecidos

--- bot_handlers.py
+++ bot_handlers.py
@@ -8,7 +8,7 @@ from utils import bot_utils
 import bot_functions
 
 
-# --- start, help and unknown comands ---
+# --- start and help comands ---
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, 
@@ -18,14 +18,8 @@ def help_command(update, context):
     update.message.reply_text(bot_messages.helpMessage)
 
 
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, 
-                             text=bot_messages.unknown)
-
-
 dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(CommandHandler('help', help_command))
-dispatcher.add_handler(MessageHandler(Filters.command, unknown))
 
 
 # --- plots ---
@@ -214,3 +208,13 @@ plot_conversation_handler = ConversationHandler(
 )                
 
 dispatcher.add_handler(plot_conversation_handler)
+
+
+# --- unknown comands ---
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, 
+                             text=bot_messages.unknown)
+
+
+dispatcher.add_handler(MessageHandler(Filters.command, unknown))
